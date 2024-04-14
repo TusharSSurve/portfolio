1451661f8f6f6439b8eaf6e99ec2d8fe99825f94
Tushar Surve <ttusharsurve567@gmail.com>
2024-04-14 19:48:57 +0530

    [master] : Cleaning Up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello
         </p>
