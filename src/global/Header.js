74dcdf7ccebaedd20c44cfc2bff473fbcf2b59d5
Tushar Surve <ttusharsurve567@gmail.com>
2024-04-18 23:28:19 +0530

    [test] : Header Desktop Completed

--- src/global/Header.js
+++ src/global/Header.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from './Header.module.css'
 import resume from '../assets/resume.pdf';
 import Button from "./Button";
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(window.pageYOffset > 0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.pageYOffset > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
-      <div className={styles['logo-container']}>
-        <a href="/">Tushar Surve</a>
-      </div>
-      <div className={styles['navbar-container']}>
-        <nav>
-          <ul>
-            <li><a href="/">ABOUT</a></li>
-            <li><a href="/">EXPERIENCE</a></li>
-            <li><a href="/">WORK</a></li>
-            <li><a href="/">CONTACT</a></li>
-          </ul>
-          <Button>
-            <a href={resume} className={styles.button} target="_blank" rel="noopener noreferrer">Resume</a>
-          </Button>
-        </nav>
+    <header className={`${styles['header']} ${isScrolled ? styles['sticky'] : ''}`}>
+      <div className={styles['header-container']}>
+        <div className={styles['logo-container']}>
+          <a href="/">TUSHAR SURVE</a>
+        </div>
+        <div className={styles['navbar-container']}>
+          <nav>
+            <ul>
+              <li><a href="#about">ABOUT</a></li>
+              <li><a href="#experience">EXPERIENCE</a></li>
+              <li><a href="#work">WORK</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+            </ul>
+            <Button>
+              <a href={resume} className={styles.button} target="_blank" rel="noopener noreferrer">RESUME</a>
+            </Button>
+          </nav>
+        </div>
       </div>
     </header>
   );
