be7c741e8eda1342430b4d5d65026021fa7041eb
Tushar Surve <ttusharsurve567@gmail.com>
2024-04-18 20:45:51 +0530

    [test] : Added Design for Footer

--- src/global/Footer.js
+++ src/global/Footer.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faGithub, faKaggle, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
     <footer>
-      <p className={styles.copyright}><FontAwesomeIcon icon={faCopyright} /> Tushar Surve 2024</p>
+      <div className={styles['footer-container']}>
+        <p className={styles.copyright}><FontAwesomeIcon icon={faCopyright} /> Tushar Surve 2024</p>
+        <div className={styles.contact}>
+          <ul>
+            <li><a href="https://github.com/TusharSSurve"><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a href="https://www.linkedin.com/in/surve-tushar/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            <li><a href="https://www.kaggle.com/tusharsurve"><FontAwesomeIcon icon={faKaggle} /></a></li>
+            <li><a href="https://instagram.com/surve_tushar"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/global/Header.js
+++ src/global/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import styles from './Header.module.css'
+import resume from '../assets/resume.pdf';
+import Button from "./Button";
 const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <div className={styles['logo-container']}>
+        <a href="/">Tushar Surve</a>
+      </div>
+      <div className={styles['navbar-container']}>
+        <nav>
+          <ul>
+            <li><a href="/">ABOUT</a></li>
+            <li><a href="/">EXPERIENCE</a></li>
+            <li><a href="/">WORK</a></li>
+            <li><a href="/">CONTACT</a></li>
+          </ul>
+          <Button>
+            <a href={resume} className={styles.button} target="_blank" rel="noopener noreferrer">Resume</a>
+          </Button>
+        </nav>
+      </div>
     </header>
   );
 }
