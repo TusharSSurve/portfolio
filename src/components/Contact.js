d713ab789e9c5508c459a09ee31a277b2ab625f9
Tushar Surve <ttusharsurve567@gmail.com>
2024-04-27 21:57:52 +0530

    [test] : Bug Fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,12 @@ import styles from './Contact.module.css';
 import Button from "../global/Button";
 const Contact = () => {
   return (
-    <div className={styles.contact}>
+    <section className={styles.contact} id="contact">
       <h2>Get In Touch</h2>
       <p>Feel free to shoot me an email whether you have a question, just want to chat, I'll do my best to get back to you!</p>
       <Button className='big'>
         <a href="mailto:ttusharsurve567@gmail.com" rel="noopener noreferrer" target="_blank">Say Hello</a>
       </Button>
-    </div>);
+    </section>);
 }
 export default Contact;
