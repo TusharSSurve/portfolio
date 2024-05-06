3d5121dd3a559dafc3f6f651113355d1c0d28b80
Tushar Surve <ttusharsurve567@gmail.com>
2024-05-06 10:13:37 +0530

    [test] : GH pages issue#2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import "./App.css";
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import Homepage from "./pages/Homepage";
 import BookReads from "./pages/BookReads";
 import Root from "./pages/Root";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: "/portfolio", element: <Homepage /> },
-      { path: "/", element: <Homepage /> },
-      { path: "/read-books", element: <BookReads /> },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       { index: true, path: "/portfolio", element: <Homepage /> },
+//       { path: "/", element: <Homepage /> },
+//       { path: "/read-books", element: <BookReads /> },
+//     ],
+//   },
+// ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return <BrowserRouter basename="/portfolio">
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Homepage />} />
+        <Route path="read-books" element={<BookReads />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
